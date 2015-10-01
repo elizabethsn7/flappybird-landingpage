481b1cc3271a5a8a9d8905bddd3e81579a2201ec
Elizabeth Kipp <liz@elizabethkipp.com>
2015-10-01 09:36:27 -0500

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -24,3 +24,9 @@ gulp.task('sass', function() {
 	.pipe(sass())
 	.pipe(gulp.dest('site/css'));
 });
+
+//Watch task
+gulp.task('watch', function() {
+	gulp.watch('site/js/*.js', ['jshint']);
+	gulp.watch('site/scss/*.scss', ['sass']);
+});
