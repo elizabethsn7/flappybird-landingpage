128189025ab5c5f381bd4c3a1172a018a538e627
Elizabeth Kipp <liz@elizabethkipp.com>
2015-10-01 12:12:58 -0500

    gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('html', function() {
 
 // JavaScript build task, removes whitespace and concatenates all files
 gulp.task('scripts', function() {
-  return browserify('./site/js/main.js').
+  return browserify('./site/js/main.js')
     .bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
